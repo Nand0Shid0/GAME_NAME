566fb9561a02ed5ea5f871f36bd6a13557b63bf2
REAL_NAME = <83304047+Nand0Shid0@users.noreply.github.com>
2021-11-12 10:50:36 -0600

    Update BoardManager.cs

--- BoardManager.cs
+++ BoardManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BoardManager : MonoBehaviour
 {
-    public int colums = 8;
+    public int columns = 8;
     public int rows = 8;
 
     public GameObject[] floorTiles, outerWallTiles; //Losetas
@@ -15,27 +15,33 @@ public class BoardManager : MonoBehaviour
     {
         BoardSetup();
     }
-    void BoardSetup()
-    {
-        new GameObject("Board");
 
-        for (int x = -1; x<colums + 1; x++)
-        {
-            for (int y= -1; x <rows+1; y++)
-            {
-                GameObject toInstantiate = GetRandomInArray(floorTiles);
+void BoardSetup()
+	{
+		//Instantiate Board and set boardHolder to its transform.
+		boardHolder = new GameObject("Board").transform;
 
-                if ( x==-1 || y == -1 || x == colums || y == rows)
-                {
-                    toInstantiate = GetRandomInArray(outerWallTiles);
-                }
-                Instantiate(toInstantiate, new Vector2(x, y), Quaternion.identity);
-            }
-        }
-    }
+		//Loop along x axis, starting from -1 (to fill corner) with floor or outerwall edge tiles.
+		for (int x = -1; x < columns + 1; x++)
+		{
+			//Loop along y axis, starting from -1 to place floor or outerwall tiles.
+			for (int y = -1; y < rows + 1; y++)
+			{
+				//Choose a random tile from our array of floor tile prefabs and prepare to instantiate it.
+				GameObject toInstantiate = floorTiles[Random.Range(0, floorTiles.Length)];
 
-    GameObject GetRandomInArray (GameObject[] array)
-    {
-        return array[Random.Range(0, array.Length)];
-    }
+				//Check if we current position is at board edge, if so choose a random outer wall prefab from our array of outer wall tiles.
+				if (x == -1 || x == columns || y == -1 || y == rows)
+					toInstantiate = outerWallTiles[Random.Range(0, outerWallTiles.Length)];
+
+				//Instantiate the GameObject instance using the prefab chosen for toInstantiate at the Vector3 corresponding to current grid position in loop, cast it to GameObject.
+				GameObject instance =
+					Instantiate(toInstantiate, new Vector3(x, y, 0f), Quaternion.identity) as GameObject;
+
+				//Set the parent of our newly instantiated object instance to boardHolder, this is just organizational to avoid cluttering hierarchy.
+				instance.transform.SetParent(boardHolder);
+			}
+		}
+	}
+
 }
